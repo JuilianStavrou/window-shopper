782f9c3ab89f41d8ff7b3681449a206acd22d216
Juilian Stavrou <juilian.stavrou@bigpond.com>
2018-04-13 10:53:04 +1000

    FIRST

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Code on 12/4/18.
+//  Copyright © 2018 Code. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    //test
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9601245241, green: 0.5569934262, blue: 0.01235326537, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+
+    @objc func calculate() {
+        print("We got here")
+        
+    }
+    
+
+}
+
